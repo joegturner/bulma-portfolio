696e5257c2564a8ccf57589197ab196967c4e4d6
joegturner <59180847+joegturner@users.noreply.github.com>
2020-06-03 15:38:46 -0400

    Updates

---
+++ component---src-pages-index-js-1ca981a32bbc2195c1f5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,t,n){"use strict";n.r(t),n.d(t,"projectsQuery",(function(){return s}));var a=n("q1tI"),o=n.n(a),r=n("Bl7J"),s="3567617861";t.default=function(e){var t=e.data,n=t.allDataJson.edges[0].node.projects,a=[];return a.push(t.file1),a.push(t.file2),o.a.createElement(r.a,{projects:n,aboutPics:a})}}}]);
+//# sourceMappingURL=component---src-pages-index-js-1ca981a32bbc2195c1f5.js.map
